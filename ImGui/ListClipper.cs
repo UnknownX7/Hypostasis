3855fce735136fdf58f4a0857815214328ba70a3
UnknownX7 <UnknownX7@users.noreply.github.com>
2023-02-12 05:54:26 -0600

    Add ListClipper

---
+++ ImGui/ListClipper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImGuiNET;
+
+public static partial class ImGuiEx
+{
+    public unsafe class ListClipper : IEnumerable<(int, int)>, IDisposable
+    {
+        private ImGuiListClipperPtr clipper;
+        private readonly int rows;
+        private readonly int columns;
+        private readonly bool twoDimensional;
+        private readonly int itemRemainder;
+
+        public int CurrentRow { get; private set; }
+        public bool IsStepped => CurrentRow == DisplayStart;
+        public int DisplayStart => clipper.DisplayStart;
+        public int DisplayEnd => clipper.DisplayEnd;
+
+        public IEnumerable<int> Rows
+        {
+            get
+            {
+                while (clipper.Step()) // Supposedly this calls End()
+                {
+                    for (int i = clipper.DisplayStart; i < clipper.DisplayEnd; i++)
+                    {
+                        CurrentRow = i;
+                        yield return twoDimensional ? i : i * columns;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<int> Columns
+        {
+            get
+            {
+                var cols = (itemRemainder == 0 || rows != DisplayEnd || CurrentRow != DisplayEnd - 1) ? columns : itemRemainder;
+                for (int j = 0; j < cols; j++)
+                    yield return j;
+            }
+        }
+
+        public ListClipper(int items, int cols = 1, bool twoD = false, float itemHeight = 0)
+        {
+            twoDimensional = twoD;
+            columns = cols;
+            rows = twoDimensional ? items : (int)MathF.Ceiling((float)items / columns);
+            itemRemainder = !twoDimensional ? items % columns : 0;
+            clipper = new ImGuiListClipperPtr(ImGuiNative.ImGuiListClipper_ImGuiListClipper());
+            clipper.Begin(rows, itemHeight);
+        }
+
+        public IEnumerator<(int, int)> GetEnumerator() => (from i in Rows from j in Columns select (i, j)).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public void Dispose()
+        {
+            clipper.Destroy(); // This also calls End() but I'm calling it anyway just in case
+            GC.SuppressFinalize(this);
+        }
+    }
+}
