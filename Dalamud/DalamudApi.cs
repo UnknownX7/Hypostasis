fd8504818ed2d2fedc207b1e3f3694b99bc4d54d
UnknownX7 <UnknownX7@users.noreply.github.com>
2023-04-06 18:53:44 -0500

    Add new service

--- Dalamud/DalamudApi.cs
+++ Dalamud/DalamudApi.cs
@@ -25,7 +25,6 @@ using Dalamud.Interface;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 
-// ReSharper disable CheckNamespace
 // ReSharper disable UnusedAutoPropertyAccessor.Local
 
 namespace Hypostasis.Dalamud;
@@ -33,107 +32,81 @@ namespace Hypostasis.Dalamud;
 public class DalamudApi
 {
     //[PluginService]
-    //[RequiredVersion("1.0")]
     public static DalamudPluginInterface PluginInterface { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static AetheryteList AetheryteList { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static BuddyList BuddyList { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static ChatGui ChatGui { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static ChatHandlers ChatHandlers { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static ClientState ClientState { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static CommandManager CommandManager { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static Condition Condition { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static DataManager DataManager { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static DtrBar DtrBar { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static DutyState DutyState { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static FateTable FateTable { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static FlyTextGui FlyTextGui { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static Framework Framework { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static GameConfig GameConfig { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static GameGui GameGui { get; private set; }
 
-    //[PluginService]
-    //[RequiredVersion("1.0")]
-    //public static GameLifecycle GameLifecycle { get; private set; }
+    [PluginService]
+    public static GameLifecycle GameLifecycle { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static GameNetwork GameNetwork { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static GamepadState GamepadState { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static JobGauges JobGauges { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static KeyState KeyState { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static LibcFunction LibcFunction { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static ObjectTable ObjectTable { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static PartyFinderGui PartyFinderGui { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static PartyList PartyList { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     private static SigScanner sigScanner
     {
         set => SigScanner = new(value);
@@ -142,15 +115,12 @@ public class DalamudApi
     public static SigScannerWrapper SigScanner { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static TargetManager TargetManager { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static TitleScreenMenu TitleScreenMenu { get; private set; }
 
     [PluginService]
-    //[RequiredVersion("1.0")]
     public static ToastGui ToastGui { get; private set; }
 
     public DalamudApi() { }
