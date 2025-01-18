482cb5939b4d75d238828ba4e609adb8d2103f3b
Caraxi <git@caraxian.com>
2025-01-19 02:16:27 +1030

    Create EmoteController.cs

---
+++ Game/Structures/EmoteController.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Hypostasis.Game.Structures;
+
+[StructLayout(LayoutKind.Explicit)]
+public unsafe struct EmoteController : IHypostasisStructure {
+    public delegate Bool CancelEmote(EmoteController* emoteController, nint unknown);
+    public static readonly GameFunction<CancelEmote> cancelEmote = new("E8 ?? ?? ?? ?? 48 8B 7B 08 45 33 C0");
+    
+    public bool Validate() => true;
+}
